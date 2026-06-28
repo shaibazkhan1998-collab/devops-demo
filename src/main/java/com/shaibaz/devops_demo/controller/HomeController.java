ba8ebd1e700ddbbe0f5710091159559cf0065d7b
shaibazkhan1998-collab <shaibazkhan1998@gmail.com>
2026-06-28 15:40:05 +0530

    Initial Spring Boot Application

---
+++ src/main/java/com/shaibaz/devops_demo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.shaibaz.devops_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello DevOps!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is Healthy";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Version 1.0";
+    }
+}
